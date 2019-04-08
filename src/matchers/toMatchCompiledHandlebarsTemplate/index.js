6db42c06f9392fd4fa527ed3d7c70c9aec2ef0c6
dawiddominiak <poczta@dawiddominiak.pl>
2019-04-08 23:57:11 +0200

    Fix stringification of data

--- src/matchers/toMatchCompiledHandlebarsTemplate/index.js
+++ src/matchers/toMatchCompiledHandlebarsTemplate/index.js
@@ -1,26 +1,29 @@
 import predicate from './predicate';
 import { resolve } from 'path';
 
+function stringifyData(data) {
+  let stringifiedData = data;
+
+  if (typeof data === 'object') {
+    stringifiedData = JSON.stringify(data, undefined, '\t');
+  }
+
+  return stringifiedData;
+}
+
 export default (viewsPath) => {
   return (html, templateName, data) => {
     const templatePath = resolve(viewsPath, templateName);
     if (predicate(html, { templatePath, data })) {
       return {
-        message: () => {
-          let stringifiedData = data;
-
-          if (typeof data === 'object') {
-            stringifiedData = JSON.stringify(data, undefined, '\t');
-          }
-
-          return `expected ${html} to not be compiled from ${templateName} for data ${stringifiedData}`;
-        },
+        message: () =>
+          `expected ${html} to not be compiled from ${templateName} for data ${stringifyData(data)}`,
         pass: true,
       };
     } else {
       return {
         message: () =>
-          `expected ${html} to be compiled from ${templateName} for data ${data}`,
+          `expected ${html} to be compiled from ${templateName} for data ${stringifyData(data)}`,
         pass: false,
       };
     }
